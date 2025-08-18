68796d70ef4fba005b194f38182be4473decf920
MarcUrquizu <marc.urquizu.c@gmail.com>
2025-08-18 21:25:51 +0200

    style: move touch controls to screen sides

--- touch-controls.js
+++ touch-controls.js
@@ -5,21 +5,19 @@
     const container = document.createElement('div');
     container.id = 'touch-controls';
     container.innerHTML = `
-      <div class="tc-row">
-        <button class="tc-btn tc-up">↑</button>
-      </div>
-      <div class="tc-row">
+      <div class="tc-left-controls">
         <button class="tc-btn tc-left">←</button>
-        <button class="tc-btn tc-down">↓</button>
         <button class="tc-btn tc-right">→</button>
+      </div>
+      <div class="tc-right-controls">
+        <button class="tc-btn tc-jump">↑</button>
       </div>
     `;
     document.body.appendChild(container);
     const map = {
-      'tc-up': 'ArrowUp',
-      'tc-down': 'ArrowDown',
       'tc-left': 'ArrowLeft',
-      'tc-right': 'ArrowRight'
+      'tc-right': 'ArrowRight',
+      'tc-jump': 'ArrowUp'
     };
     Object.keys(map).forEach(cls => {
       const btn = container.querySelector('.' + cls);
